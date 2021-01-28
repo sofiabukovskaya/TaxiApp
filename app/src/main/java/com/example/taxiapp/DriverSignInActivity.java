aa7c849a8ce4bf0fe797931c3b879a421533d905
sofiabukovskaya <sofi454545@gmail.com>
2021-01-28 23:12:54 +0200

    Add Google maps and Location API

--- app/src/main/java/com/example/taxiapp/DriverSignInActivity.java
+++ app/src/main/java/com/example/taxiapp/DriverSignInActivity.java
@@ -3,6 +3,7 @@ package com.example.taxiapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -116,6 +117,7 @@ public class DriverSignInActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "createUserWithEmail:success");
+                                startActivity(new Intent(DriverSignInActivity.this, DriverMapsActivity.class));
                                 FirebaseUser user = auth.getCurrentUser();
                                 //updateUI(user);
                             } else {
@@ -141,6 +143,7 @@ public class DriverSignInActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "createUserWithEmail:success");
+                                startActivity(new Intent(DriverSignInActivity.this, DriverMapsActivity.class));
                                 FirebaseUser user = auth.getCurrentUser();
                                 //updateUI(user);
                             } else {
